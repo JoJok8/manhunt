13991f81c3e5cda69b0a97c0b91816ae3dc15228
Isaac Wasserman <isaacrwasserman@gmaill.com>
2018-08-21 03:17:55 -0400

    Added Tab Nav

--- Manhunt/Content/Pages/IT_Feed.js
+++ Manhunt/Content/Pages/IT_Feed.js
@@ -1,12 +1,9 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class IT_Feed extends Component {
-  static navigationOptions = {
-    tabBarIcon: <Icon name="rocket" size={30} color="#900" />
-  }
 
   render() {
     return (
